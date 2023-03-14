876e60b3f7ed12ab4d70cd8465f2654ee20dab72
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2023-03-14 11:49:26 +0700

    Challenge 2 revised

--- challenge2.go
+++ challenge2.go
@@ -11,9 +11,9 @@ func main() {
 
 	for j := 0; j < 11; j++ {
 		if j == 5 {
-			var character = [7]rune{'С', 'А', 'Ш', 'А', 'Р', 'В', 'О'}
-			for k := 0; k < len(character); k++ {
-				fmt.Printf("character %U '%c' starts at byte position %d\n", character[k], character[k], 2*k)
+			character := "САШАРВО"
+			for pos, char := range character {
+				fmt.Printf("character %U '%c' starts at byte position %d\n", char, char, pos)
 			}
 			continue
 		}
